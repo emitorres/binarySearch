f801ee9def9a60501d1ca3c7f3b7e977840219c6
emitorres93 <emitorres93@gmail.com>
2021-10-23 21:12:44 -0300

    Add static import

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,12 @@
 package main;
 
-import main.utils.Utils;
+import static main.utils.Utils.binarySearch;
 
 public class Main {
     public static void main(String[] args) {
         int [] arreglo = new int []{1,4,3,2,5,30,41,8,50,80};
         int numberToFind = 2;
-        int pos = Utils.binarySearch(arreglo, numberToFind);
+        int pos = binarySearch(arreglo, numberToFind);
         System.out.println("The number " + numberToFind +
                            " is in the " + pos + " position");
     }
